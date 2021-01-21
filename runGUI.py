1daffd90f1cb198c506047f078fb88e44ab3523a
Metin Bicer <m.bicer19@imperial.ac.uk>
2021-01-21 00:14:48 +0000

    formatting and commenting

--- runGUI.py
+++ runGUI.py
@@ -2,12 +2,17 @@
 """
 Created on Tue Jan  19 14:30:02 2020
 
+-runs multiple so and jr analyses from the GUI.
+-run this file from the GUI of OpenSim 3.3.
+
+1. update mainFold (the path to the folder containing this script) which includes username etc.
+2. change trial (the name of the folder under mainFold/Results)
+
 @author: metin
 """
 import os
 # main folder
 mainFold = 'C:/Users/metin/Desktop/PhD/MaxIso_ESB'
-# method (WeakHip, WeakKnee, WeakAnkle, WeakFull)
 resultsFolder = 'Results'
 # define the folder fullpath
 analysisFold = mainFold + '/' + resultsFolder
@@ -23,16 +28,19 @@ grfFile = idFold + '/grf.mot'
 # read files
 ik = modeling.Storage(ikFile)
 grf = modeling.Storage(grfFile)
-# model
+# read the original (scaled) model
 osimModel = modeling.Model(modelFile)
 osimModel.initSystem()
 # xml files
 forcesetFile = mainFold + '/Forceset.xml'
 externalLoadsFile = mainFold + '/ExternalLoads.xml'
+# get the ExternalLoads object to write the exact path of the grfFile
 extLoads = modeling.ExternalLoads(osimModel, externalLoadsFile)
 extLoads.setDataFileName(grfFile)
 externalLoadsFile = trialFold + '/ExternalLoads.xml'
+# print the external loads under trialFold
 extLoads.print(trialFold + '/ExternalLoads.xml')
+# get the SO analysis
 soAnalysisFile = mainFold + '/SO_Analysis.xml'
 soAnalysis = modeling.StaticOptimization()
 soAnalysis.setStartTime(grf.getFirstTime())
@@ -41,7 +49,7 @@ soAnalysis.setEndTime(grf.getLastTime())
 soResultFolder = analysisFold + '/SOResults'
 jrResultFolder = analysisFold + '/JRResults'
 
-# soTool
+# create a soTool (this will be updated for each analysis)
 soTool = modeling.AnalyzeTool()
 soTool.getAnalysisSet().adoptAndAppend(soAnalysis)
 soTool.setInitialTime(grf.getFirstTime())
@@ -54,10 +62,13 @@ soTool.setCoordinatesFileName(ikFile)
 soTool.setLowpassCutoffFrequency(6)
 soTool.setResultsDir(soResultFolder)
 
-#jranalysis
+# create a jr analysis (this will be updated for each analysis)
 jrAnalysis = modeling.JointReaction()
 jrAnalysis.setName('JR')
-# names
+jrAnalysis.setStartTime(grf.getFirstTime())
+jrAnalysis.setEndTime(grf.getLastTime())
+# joint reactions to be calculated for jointNames exerted on
+# onBodies and expressed in inFrame
 jointNames = modeling.ArrayStr()
 onBodies = modeling.ArrayStr()
 inFrame = modeling.ArrayStr()
@@ -68,12 +79,10 @@ for joint, body, frame in zip(jrJointNames, jrOnBodies, jrInFrame):
     jointNames.append(joint)
     onBodies.append(body)
     inFrame.append(frame)
-jrAnalysis.setStartTime(grf.getFirstTime())
-jrAnalysis.setEndTime(grf.getLastTime())
 jrAnalysis.setOnBody(onBodies)
 jrAnalysis.setInFrame(inFrame)
 jrAnalysis.setJointNames(jointNames)
-# jrtool
+# create a jr tool (this will be updated for each analysis)
 jrTool = modeling.AnalyzeTool()
 jrTool.setResultsDir(jrResultFolder)
 jrTool.setInitialTime(grf.getFirstTime())
@@ -82,27 +91,31 @@ jrTool.setExternalLoadsFileName(externalLoadsFile)
 jrTool.setCoordinatesFileName(ikFile)
 jrTool.setLowpassCutoffFrequency(6)
 
+# keys are the model names and values are the joint names (that are
+# used to obtain muscle groups crossing these joints)
 groupNames = {'WeakHip': ['hip'],
               'WeakKnee': ['knee'],
               'WeakAnkle': ['ankle'],
-              'WeakFull': ['hip', 'knee', 'ankle'],
-              'WeakFlexExt': ['flex', 'ext', 'ankle']
-              }
-
+              'WeakFull': ['hip', 'knee', 'ankle']}
+# % change in the reduction of the maximum isometric forces
 for change in [10, 20, 30, 40, 0, -10, -20, -30, -40]:
-    for method, groups in groupNames.items():
+    # for each model and its muscle groups
+    for modelName, groups in groupNames.items():
+        # load unmodified model
         osimModel = modeling.Model(modelFile)
         osimModel.initSystem()
+        # set the model name depending on the change
         if change == 0:
-            osimModel.setName(method+'_'+trial)
+            osimModel.setName(modelName+'_'+trial)
         else:
-            osimModel.setName(method+'_'+trial+["", "+"][change > 0] + str(change))
+            osimModel.setName(modelName+'_'+trial+["", "+"][change > 0] + str(change))
+        # get forcesets to update muscle max isometric forces
         forceset = osimModel.getForceSet()
         rGroupNames = modeling.ArrayStr()
         forceset.getGroupNames(rGroupNames)
-
         muscleGroupIndex = []
         muscleGroupNames = []
+        # list of muscle names (to be used for changing the max isometric forces)
         muscleNames = []
         for i in range(rGroupNames.getSize()):
             gname = rGroupNames.get(i)
@@ -116,31 +129,35 @@ for change in [10, 20, 30, 40, 0, -10, -20, -30, -40]:
                 for name in groupMembersStr:
                     if name not in muscleNames:
                         muscleNames.append(name)
-
+        # a writable reference to the model muscles
         updMuscles = osimModel.updMuscles()
-        newForces = []
         for name in muscleNames:
             muscleAbstract = updMuscles.get(name)
             muscle = modeling.Millard2012EquilibriumMuscle().safeDownCast(muscleAbstract)
-            
+            # unmodified muscle max isometric force
             previousMaxIsoForce = muscle.getMaxIsometricForce()
-            newForces.append(previousMaxIsoForce*(1+change/100.0))
+            # update the max isometric force
             muscle.setMaxIsometricForce(previousMaxIsoForce*(1+change/100.0))
-    
+        # print to a new modelfile
         newModelFile = trialFold + '/' + osimModel.getName()+'.osim'
         osimModel.print(newModelFile)
+        # set soTool attribs depending on this new model
         soTool.setName(osimModel.getName())
         soTool.setModelFilename(newModelFile)
-        soTool.print('a.xml')
-        soTool_run = modeling.AnalyzeTool('a.xml')
+        # print the soTool (prints to the OpenSim installation directory (C:\OpenSim 3.3)
+        soTool.print('so.xml')
+        # create a new soTool from the printed xml
+        soTool_run = modeling.AnalyzeTool('so.xml')
         soTool_run.run()
         
         # jrAnalysis
         jrAnalysis.setForcesFileName(soResultFolder + '/' + osimModel.getName() + '_StaticOptimization_force.sto')
-        # jrtool
+        # set jrtool attribs depending on this new model
         jrTool.setModelFilename(newModelFile)
         jrTool.getAnalysisSet().adoptAndAppend(jrAnalysis)
         jrTool.setName(osimModel.getName())
-        jrTool.print('b.xml')
-        jrTool_run = modeling.AnalyzeTool('b.xml')
+        # print the jrTool (prints to the OpenSim installation directory (C:\OpenSim 3.3)
+        jrTool.print('jr.xml')
+        # create a new soTool from the printed xml
+        jrTool_run = modeling.AnalyzeTool('jr.xml')
         jrTool_run.run()
