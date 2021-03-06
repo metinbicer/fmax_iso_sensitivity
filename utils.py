a95e1979692f8090a3fb4eb5d920f1e8bc40ef2a
Metin Bicer <m.bicer19@imperial.ac.uk>
2021-03-06 15:11:21 +0000

    add functions to load, save JRFs, and delete plots

--- utils.py
+++ utils.py
@@ -8,12 +8,13 @@ import opensim as osim
 import numpy as np
 import os
 from scipy import interpolate
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import copy
 import pandas as pd
 from scipy import signal
-plt.style.use('fivethirtyeight')
+from scipy.io import loadmat
+try:
+    import cPickle as pickle
+except ImportError:  # Python 3.x
+    import pickle
 
 # jr loads
 JR_LOADS = {
@@ -23,12 +24,12 @@ JR_LOADS = {
                       'hip_l_on_femur_l_in_femur_l_mx',
                       'hip_l_on_femur_l_in_femur_l_my',
                       'hip_l_on_femur_l_in_femur_l_mz']),
-    'knee': np.array(['knee_l_on_tibia_l_in_tibia_l_fx',
-                      'knee_l_on_tibia_l_in_tibia_l_fy',
-                      'knee_l_on_tibia_l_in_tibia_l_fz',
-                      'knee_l_on_tibia_l_in_tibia_l_mx',
-                      'knee_l_on_tibia_l_in_tibia_l_my',
-                      'knee_l_on_tibia_l_in_tibia_l_mz']),
+    'knee': np.array(['walker_knee_l_on_tibia_l_in_tibia_l_fx',
+                      'walker_knee_l_on_tibia_l_in_tibia_l_fy',
+                      'walker_knee_l_on_tibia_l_in_tibia_l_fz',
+                      'walker_knee_l_on_tibia_l_in_tibia_l_mx',
+                      'walker_knee_l_on_tibia_l_in_tibia_l_my',
+                      'walker_knee_l_on_tibia_l_in_tibia_l_mz']),
     'ankle': np.array(['ankle_l_on_talus_l_in_talus_l_fx',
                      'ankle_l_on_talus_l_in_talus_l_fy',
                      'ankle_l_on_talus_l_in_talus_l_fz',
@@ -37,36 +38,76 @@ JR_LOADS = {
                      'ankle_l_on_talus_l_in_talus_l_mz'])
     }
 
-# converts osim.Storage file into dict of numpy arrays
-def stoToNumpy(file):
-    storage = osim.Storage(file)
-    # number of columns and rows in storage
-    numCol = storage.getColumnLabels().getSize()
-    numRow = storage.getSize()
+# loads in-vivo JRFs from the saved file (divide it by scaling)
+def loadExpJRF(scaling=1):
+    # force names in mat file and output dict
+    forceNames = {'F_medial': 'medial', 'F_lateral': 'lateral', 'F_total_magnitude': 'knee'}
+    try:
+        # read the mat file
+        mat = loadmat('eTibia_data.mat')
+    except:
+        print('Exp JRFs are not saved')
+    # read the structured mat to dicts
+    expReactions = {} # its keys are the trial names in the mat file
+    eTibia_data = mat['eTibia_data'] # eTibia_data structure of the mat file
+    # for each experiment (e.g. 'GC5'
+    for expNo in list(eTibia_data.dtype.names):
+        exp = eTibia_data[0,0][expNo][0,0]['ngait_og'][0,0] # mat structure for the experiment (expNo)
+        data = exp['data'] # its data
+        trial_names = exp['trial_names'][0] # trial names in the exp
+        colheaders = exp['colheaders'][0] # column names (forces) in the exp
+        # each trial of the exp
+        for idx_trial, trial in enumerate(trial_names):
+            trial = trial.astype(str)[0] #string
+            # find the trial name (e.g. 'ss1')
+            s1 = trial.find('_s')
+            s2 = trial.find('.')
+            trial = trial[s1+1:s2]
+            # exp and trial name (e.g. 'GC5_ss1')
+            expTrial = expNo + '_' + trial
+            # each trial of an experiment is a dict containing the JRFs
+            expReactions[expTrial] = {}
+            for idx_col, colName in enumerate(colheaders):
+                colName = colName[0]
+                expReactions[expTrial][forceNames[colName]] = data[:, idx_col, idx_trial]/scaling
+    return expReactions
 
-    # storage will be saved to dict
-    dictStorage = dict()
 
-    # loop through columns
-    for col in range(numCol):
-        colName = storage.getColumnLabels().get(col)
-        # arraydouble to collect the data column from storage
-        arr = osim.ArrayDouble()
-        # save the column to arr_c
-        storage.getDataColumn(colName, arr)
-        # loop through rows to save the column
-        dictStorage[colName] = np.array([arr.get(row) for row in range(numRow)])
-
-    # return the dictionary containing the numpy arrays
-    return dictStorage
+# loads model JRFs from the saved file
+def loadModelJRF():
+    try:
+        with open('modelJRF.p', 'rb') as fp:
+            reactions = pickle.load(fp)
+            return reactions
+    except:
+        print('JRFs are not saved')
 
 
-# calculate the contact forces from the joint reaction analysis
+# reads JRF of all valid analysis and saves them
+# returns reactions (dict containing trial names as keys, and the values are the all valid analysis)
+# params:
+#   trials: list of string (results should be written using folders with the same name as trials)
+#   scaling: the parameter to scale the calculated total reaction forces
+def saveModelJRF(trials=['GC5_ss1', 'GC5_ss3', 'GC5_ss8', 'GC5_ss9', 'GC5_ss11'], BW=75*9.81):
+    # all reactions are stored in a dict whose keys are the folder names (trials)
+    reactions = {}
+    for fold in trials:
+        # joint reaction forces in a dict whose keys are the filenames (scaling=BW)
+        reactions[fold] = totalReactionForces('Results\\'+fold, scaling=BW)
+    # store the reactions for each trial
+    with open('modelJRF.p', 'wb') as fp:
+        pickle.dump(reactions, fp, protocol=pickle.HIGHEST_PROTOCOL)
+    return reactions
+
+
+# calculate the reaction forces from the joint reaction analysis
 # jrfileContent is the dictionary containing jr analysis results
-# scaling is the scale parameter for the calculated total contact forces
+# params:
+#   fold: contains the JRResults and SOResults subfolders
+#   scaling: the parameter to scale the calculated total reaction forces
 def totalReactionForces(fold, scaling=1):
     # jr results folder
-    jrResultsFold = fold+'\\JRResults'
+    jrResultsFold = fold + '\\JRResults'
     # read the jr result files in the folder
     files = [os.path.join(jrResultsFold, file) for file in os.listdir(jrResultsFold)]
     # a dict to save all reaction forces from the files
@@ -76,28 +117,33 @@ def totalReactionForces(fold, scaling=1):
         fileName = os.path.split(file)[1]
         # check whether the simulation is valid (reserve actuator moments 
         # not exceeding 5% of the ID moments)
-        _ = checkSimulation(fold, fileName)
-        # forces of a file
-        jrDict = stoToNumpy(file)
-        for key, data in jrDict.items():
-            # normalize to the gait cycle
-            jrDict[key] = Normalize2GC(data)
-        for joint, forces in JR_LOADS.items():
-            # forces in x, y and z
-            fx = jrDict[forces[0]]
-            fy = jrDict[forces[1]]
-            fz = jrDict[forces[2]]
-            # the total joint reaction force of the joint
-            jrDict[joint] = ((fx**2 + fy**2 + fz**2)**0.5)/scaling
-            # if the joint is knee, calculate the lateral and medial loads
-            if joint.lower() == 'knee':
-                tx = jrDict[forces[3]]
-                flateral = -0.997*fz + 0.506*(-fy) - 17.9*tx
-                fmedial = 0.997*fz+ 0.494*(-fy) + 17.9*tx
-                jrDict['lateral'] = np.where(flateral>0, flateral/scaling, 0)
-                jrDict['medial'] = np.where(fmedial>0, fmedial/scaling, 0)
-        # save the reaction forces to the dict with the key being the filename
-        reactionDict[fileName] = jrDict
+        if checkSimulation(fold, fileName):
+            # forces of a file
+            jrDict = stoToNumpy(file)
+            for key, data in jrDict.items():
+                # normalize to the gait cycle
+                jrDict[key] = Normalize2GC(data)
+            for joint, forces in JR_LOADS.items():
+                # forces in x, y and z
+                fx = jrDict[forces[0]]
+                fy = jrDict[forces[1]]
+                fz = jrDict[forces[2]]
+                # the total joint reaction force of the joint
+                jrDict[joint] = ((fx**2 + fy**2 + fz**2)**0.5)/scaling
+                # if the joint is knee, calculate the lateral and medial loads
+                if joint.lower() == 'knee':
+                    # if the joint is knee, calculate lateral and medial forces
+                    tx = jrDict[forces[3]]
+                    flateral = -0.997*fz + 0.506*(-fy) - 17.9*tx
+                    fmedial = 0.997*fz+ 0.494*(-fy) + 17.9*tx
+                    jrDict['lateral'] = np.where(flateral>0, flateral/scaling, 0)
+                    jrDict['medial'] = np.where(fmedial>0, fmedial/scaling, 0)
+            # save the reaction forces to the dict with the key being the filename
+            reactionDict[fileName] = jrDict
+        # if exceeding 5%
+        else:
+            print('{} not a valid simulation'.format(file))
+
     return reactionDict
 
 # normalizes the given data into 0-100 (gaitcycle)
@@ -124,14 +170,14 @@ def checkSimulation(fold, jrFileName):
     # get the name of the model and subject and the amount of reduction in max iso
     model, subj, reduction = analysisDetails(jrFileName)
     # so results are saved with this prefix
-    analysis = model + '_' + subj + reduction
+    analysis = model + reduction + '_' + subj
     soFileName = analysis + '_StaticOptimization_force.sto'
     # full path to the sofile
     soFile = fold + '\\SOResults\\' + soFileName
     # dict containing the so results
     soNumpy = stoToNumpy(soFile)
     # id results filename and dict containing the results
-    idFile = fold + '\\4_Inverse_Dynamics\\Results\\id_GenForces.sto'
+    idFile = fold + '/id.sto'
     idNumpy = stoToNumpy(idFile)
 
     # check hip, knee and ankle reserve actuators
@@ -175,112 +221,40 @@ def analysisDetails(jrFileName):
     # remove model name from the filename
     jrFileName = jrFileName.replace(model+'_','')
     # find the reduction amount (it can be +)
-    s = jrFileName.find('+')
+    s = model.find('+')
     # if not +, try -
-    if s == -1: s = jrFileName.find('-')
+    if s == -1: s = model.find('-')
     # if there is no reduction
     if s == -1:
         reduction = ''
     else:
         # get the reduction amount
-        e = jrFileName.find('_', s)
-        reduction = jrFileName[s:e]
-        jrFileName = jrFileName.replace(reduction,'')
+        reduction = model[s:]
+        model = model[:s]
     # subject (and/or trial) name
     subj = jrFileName[0:jrFileName.find('_JR')]
     return model, subj, reduction
 
 
-# creates figure and returns the handles for the figure and the axes
-# to plot the total reaction forces on the hip, knee and ankle
-def createFigure(nrows, ncols, ylabels):
-    # create figure and axes with the given number of rows and cols
-    fig, axs = plt.subplots(nrows, ncols, sharex=True, sharey=False)
-    fig.patch.set_facecolor('white')
-    # set some properties of the axes
-    for ax in axs.flatten():
-        ax.set_xlim([0, 100])
-        ax.set_facecolor('white')
-        ax.set_ylim([0, 6])
-    # y labels
-    for r in range(nrows):
-        axs[r, 0].set_ylabel(ylabels[r])
-    # x labels
-    for ax in axs[-1,:]: ax.set_xlabel('% Gait Cycle')
+# converts osim.Storage file into dict of numpy arrays
+def stoToNumpy(file):
+    storage = osim.Storage(file)
+    # number of columns and rows in storage
+    numCol = storage.getColumnLabels().getSize()
+    numRow = storage.getSize()
 
-    return fig, axs
+    # storage will be saved to dict
+    dictStorage = dict()
 
-# get the current fig and ax
-# order labels depending on the max iso percent change
-# adjust the subplots
-def arrangeFigure():
-    # get current fig and ax
-    fig, ax = plt.gcf(), plt.gca()
-    # get the handles and labels
-    handles, labels = ax.get_legend_handles_labels()
-    # order the labels
-    labelDict = {}
-    for label, handle in zip(labels, handles):
-        labelDict[label] = handle
-    reductions = [int(l[:-1]) if not 'Nom' in l else 0 for l in labels]
-    reductions.sort(reverse=False)
-    labels = [["", "+"][r>0]+str(r)+'%' if r else 'Nominal' for r in reductions]
-    # order the handles accordingly
-    handles = [labelDict[label] for label in labels]
-    # set the figure legend
-    # copy the handles
-    handles = [copy.copy(ha) for ha in handles]
-    # set the linewidths to the copies
-    [ha.set_linewidth(7) for ha in handles ]
-    #fig.legend(handles, labels, ncol=len(labels), loc='lower center',
-    #           prop={'size': 15}, facecolor='white', edgecolor='white')
-    # adjust the subplots
-    fig.subplots_adjust(top=0.95, bottom=0.15, wspace=0.2, hspace=0.2)
-    
+    # loop through columns
+    for col in range(numCol):
+        colName = storage.getColumnLabels().get(col)
+        # arraydouble to collect the data column from storage
+        arr = osim.ArrayDouble()
+        # save the column to arr_c
+        storage.getDataColumn(colName, arr)
+        # loop through rows to save the column
+        dictStorage[colName] = np.array([arr.get(row) for row in range(numRow)])
 
-def generateFigure(reactions, rows, cols, ylabels):
-    # create the figure template
-    fig, axs = createFigure(len(rows), len(cols.keys()), ylabels)
-    n_lines = int((len(reactions.keys()) - len(cols.keys()))/len(cols.keys()))
-    # for each jr file and its content
-    for file, jrf in reactions.items():
-        # get the name of the model and the reduction amount
-        model, _, reduction = analysisDetails(file)
-        reduction, cmap, r, lw, ls = getPlotProps(reduction, n_lines)
-        # get the column of the axs for this model
-        axCol = axs[:, cols[model]]
-        # set its title
-        axCol[0].set_title(model)
-        # for each row of this column (plot each joint force)
-        for ax, row in zip(axCol, rows):
-            ax.plot(jrf[row.lower()], label=reduction, c=cmap.to_rgba(r),
-                    linewidth=lw, linestyle=ls)
-    # arrange figure, axs, labels, positions etc
-    arrangeFigure()
-    return fig, axs
-
-
-def getPlotProps(reduction, n_lines):
-    r = int(reduction) if reduction else 0
-    lw = 1
-    ls = '-'
-    if r == 0:
-        cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=n_lines),
-                                     cmap=mpl.cm.Greys_r)
-        lw = 2
-        ls = '-.'
-    elif r > 0:
-        cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=n_lines),
-                                     cmap=mpl.cm.Reds)
-        r = (abs(r)+30)/10
-    else:
-        cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=n_lines),
-                                     cmap=mpl.cm.Blues)
-        r = (abs(r)+30)/10
-    # reduction is for the label
-    if not reduction:
-        # if there is no reduction, label='Nominal'
-        reduction = 'Nominal'
-    else:
-        reduction += '%'
-    return reduction, cmap, r, lw, ls
+    # return the dictionary containing the numpy arrays
+    return dictStorage
