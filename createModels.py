bdc0558f5903dba69f7aabd154f7a899c418d20e
Metin Bicer <m.bicer19@imperial.ac.uk>
2021-03-11 10:43:44 +0000

    create models using the muscle path points (instead of muscle groupings)

--- createModels.py
+++ createModels.py
@@ -6,13 +6,15 @@ Created on Wed Mar  03 14:30:02 2020
 """
 import os
 import opensim as osim
+from itertools import combinations
+import numpy as np
 
 
 def createModels(modelFileName='Rajagopal2015-scaled.osim', 
-                 groupNames={'WeakHip': ['hip'], 
-                             'WeakKnee': ['knee'], 
-                             'WeakAnkle': ['ankle'],
-                             'WeakFull': ['hip', 'knee', 'ankle']}, 
+                 groupNames={'Hip': ['hip_l'],
+                             'Knee': ['walker_knee_l'],
+                             'Ankle': ['ankle_l'],
+                             'Full': ['hip_l', 'walker_knee_l', 'ankle_l']},
                  changeAmounts=[-40, -30, -20, -10, 0, 10, 20, 30, 40]):
     '''
     creates models with different joint strengths
@@ -34,28 +36,22 @@ def createModels(modelFileName='Rajagopal2015-scaled.osim',
     # load unmodified model
     osimModel = osim.Model(modelFile)
     osimModel.initSystem()
-    # get the forceset and its group names
-    forceset = osimModel.getForceSet()
-    rGroupNames = osim.ArrayStr()
-    forceset.getGroupNames(rGroupNames)
-    # store the muscle names in each group
-    muscleGroups = {}
-    for i in range(rGroupNames.getSize()):
-        gname = rGroupNames.get(i)
-        # no need to store group names for each legs
-        if 'leg' not in gname:
-            muscleGroup = forceset.getGroup(gname)
-            groupMembers = muscleGroup.getPropertyByIndex(0)
-            groupMembersStr = groupMembers.toString()
-            groupMembersStr = groupMembersStr.split()
-            groupMembersStr[0] = groupMembersStr[0][1:]
-            groupMembersStr[-1] = groupMembersStr[-1][:-1]
-            muscleGroups[gname] = groupMembersStr
-
+    # get the muscle names
+    muscleNames = [osimModel.getMuscles().get(i).getName() for i in range(osimModel.getMuscles().getSize())]
+    # find joints spanned by each muscle
+    muscleJoints = findJoints(osimModel, muscleNames)
     # % change in the reduction of the maximum isometric forces
     for change in changeAmounts:
         # for each model and its muscle groups
         for modelName, groups in groupNames.items():
+            # find muscles to be changed
+            musclesChanged = []
+            for muscleName, joints in muscleJoints.items():
+                 for item in joints:
+                    if item in groups:
+                 #if any(item in groups for item in joints):
+                        musclesChanged.append(muscleName)
+            musclesChanged = list(set(musclesChanged))
             # folder for this model group
             modelGroupFold = os.path.join(modelFold, modelName)
             # create a folder for each model group
@@ -73,25 +69,9 @@ def createModels(modelFileName='Rajagopal2015-scaled.osim',
             forceset = osimModel.getForceSet()
             rGroupNames = osim.ArrayStr()
             forceset.getGroupNames(rGroupNames)
-            muscleGroupIndex = []
-            muscleGroupNames = []
-            # list of muscle names (to be used for changing the max isometric forces)
-            muscleNames = []
-            for i in range(rGroupNames.getSize()):
-                gname = rGroupNames.get(i)
-                if any(g in gname.lower() for g in groups):
-                    muscleGroup = forceset.getGroup(gname)
-                    groupMembers = muscleGroup.getPropertyByIndex(0)
-                    groupMembersStr = groupMembers.toString()
-                    groupMembersStr = groupMembersStr.split()
-                    groupMembersStr[0] = groupMembersStr[0][1:]
-                    groupMembersStr[-1] = groupMembersStr[-1][:-1]
-                    for name in groupMembersStr:
-                        if name not in muscleNames:
-                            muscleNames.append(name)
             # a writable reference to the model muscles
             updMuscles = osimModel.updMuscles()
-            for name in muscleNames:
+            for name in musclesChanged:
                 muscleAbstract = updMuscles.get(name)
                 muscle = osim.Millard2012EquilibriumMuscle().safeDownCast(muscleAbstract)
                 # unmodified muscle max isometric force
@@ -102,6 +82,52 @@ def createModels(modelFileName='Rajagopal2015-scaled.osim',
             newModelFile = modelGroupFold + '/' + osimModel.getName()+'.osim'
             osimModel.printToXML(newModelFile)
     print('------------Finished creating models------------')
+
+
+def findJoints(osimModel, muscleNames):
+    # distal body (the last item in the set)
+    pairs = findChildParentPairs(osimModel)
+    muscleJoints = {}
+    for muscleName in muscleNames:
+        joints = []
+        bodyNames = findBodyNames(osimModel, muscleName)
+        bodyPairs = list(combinations(bodyNames, 2))
+        for body1, body2 in bodyPairs:
+            # first and last bodies in the list
+            while body1 != body2:
+                try:
+                    try:
+                        j = pairs.keys()[pairs.values().index([body1, body2])]
+                    except:
+                        j = pairs.keys()[pairs.values().index([body2, body1])]
+                    body2 = body1
+                    joints.append(j)
+                except:
+                    for j, bodies in pairs.items():
+                        if body2 == bodies[1]:
+                            joints.append(j)
+                            body2 = bodies[0]
+                            break
+        muscleJoints[muscleName] = list(set(joints))
+    return muscleJoints
+
+
+def findChildParentPairs(osimModel):
+    pairs = {}
+    joints = osimModel.getJointSet()
+    for i in range(joints.getSize()):
+        thisJoint = joints.get(i)
+        pairs[thisJoint.getName()] = [thisJoint.getParentName(),  # parent body
+                                      thisJoint.getBody().getName()] # child body
+    return pairs
+
+
+def findBodyNames(osimModel, muscleName):
+    thisMuscle = osimModel.getMuscles().get(muscleName)
+    ppSet = thisMuscle.getGeometryPath().getPathPointSet()
+    bodyNamesSet = np.array([ppSet.get(i).getBody().getName() for i in range(ppSet.getSize())])
+    _, idx = np.unique(bodyNamesSet, return_index=True)
+    return bodyNamesSet[np.sort(idx)]
 
 
 if __name__ == "__main__":
